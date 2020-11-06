17129ea7e6f3dbb7af2a862065e9f295760a36cc
hejie <565703319@qq.com>
2020-11-06 19:57:16 +0800

    [hejie] add divide groups api

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/entity/Student.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/entity/Student.java
@@ -12,4 +12,10 @@ public class Student {
     private String name;
     private String gender;
     private String note;
+
+    @Override
+    public Object clone() {
+        Student student = new Student(id, name, gender, note);
+        return student;
+    }
 }
--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/service/GroupService.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/service/GroupService.java
@@ -1,13 +1,19 @@
 package com.thoughtworks.capability.gtb.restfulapidesign.service;
 
 import com.thoughtworks.capability.gtb.restfulapidesign.entity.Group;
+import com.thoughtworks.capability.gtb.restfulapidesign.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class GroupService {
     private final Group[] groups = new Group[6];
+    @Autowired
+    private StudentService studentService;
 
     public GroupService() {
         for (int i = 0; i < groups.length; i++) {
@@ -19,11 +25,24 @@ public class GroupService {
         if (id < 1 || id > groups.length) {
             throw new IllegalArgumentException("没有该组");
         }
-        groups[id-1].setName(name);
+        groups[id - 1].setName(name);
     }
 
     public Group[] divideGroups() {
-
-        return new Group[0];
+        List<Student> students = studentService.getAllStudents(null);
+        int j = 0;
+        List<Student> copyStudents = new ArrayList<>();
+        for (Student student : students) {
+            copyStudents.add((Student) student.clone());
+        }
+        Collections.shuffle(copyStudents);
+        for (Student student : copyStudents) {
+            if (j >= 6) {
+                j = 0;
+            }
+            groups[j].getStudents().add(student);
+            j++;
+        }
+        return groups;
     }
 }
